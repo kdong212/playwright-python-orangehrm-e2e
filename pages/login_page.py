0e381c745b86b7791d7f29ea8cd0ce7ebaa35517
kim-dong <KDong3@slb.com>
2025-12-09 18:22:14 +0700

    upload Heroku app page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from pages.base_page import BasePage
+from pages.new_window_page import NewWindowPage
 
 class LoginPage(BasePage):
     #
@@ -8,15 +9,27 @@ class LoginPage(BasePage):
     FACEBOOK_ICON_LINK = "//a[@href='https://www.facebook.com/OrangeHRM/']"
     TWITTER_ICON_LINK = "a[href*='twitter.com/orangehrm']"
     YOUTUBE_ICON_LINK = "https://www.youtube.com/c/OrangeHRMInc"
-    YOUTUBE_PAGE_TITLE = "OrangeHRM Inc"
+    PAGE_TITLE = "OrangeHRM"
+
+    def __init__(self, page):
+        super().__init__(page)
 
     # Business Actions
     def go_to_url(self):
         self._visit(self.URL)
 
-    def open_external_link(self,external:str):
-        self._click(external)
+    def open_new_tab_and_return_new_object(self,social_name:str) -> NewWindowPage:
+        """
+        Click External link để mở tab mới
+        và trả về NewWindowPage (POM) của tab mới
+        """
+        with self.page.context.expect_page() as new_page_info:
+            self._click(social_name)
+        
+        new_page = new_page_info.value
+        new_page.wait_for_load_state()
 
-    
+        return NewWindowPage(new_page)
 
-    
+    def verify_social_link():
+        pass
